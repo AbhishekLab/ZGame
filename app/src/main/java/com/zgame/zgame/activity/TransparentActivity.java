bf105a5a5e8b10f6101f75e8a7978bbeaaf9c2b4
Abhishek kumar <abhi.cs005@gmail.com>
2020-02-18 11:41:37 +0530

    mics

--- app/src/main/java/com/zgame/zgame/activity/TransparentActivity.java
+++ app/src/main/java/com/zgame/zgame/activity/TransparentActivity.java
@@ -1,33 +1,62 @@
 package com.zgame.zgame.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import com.zgame.zgame.R;
 import static android.util.Log.d;
 
-public class TransparentActivity extends Activity {
+public class TransparentActivity extends Activity   {
 
-    private boolean isActivityAlive = false;
+    private boolean infinity = true;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transparent);
 
-        startActivityService();
+        if (permissionGranted()) {
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestNewPermissions();
+        }
 
-
-
-
-
+        //startActivityService();
 
         //findViewById(R.id.startDummyService).setOnClickListener(view -> startActivityService());
         findViewById(R.id.checkInstance).setOnClickListener(view -> stopActivityService());
 
         //finish();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestNewPermissions() {
+        this.requestPermissions(new String[] { Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE }, 100 );
+    }
+
+    private boolean permissionGranted() {
+        String storageReadPermission = Manifest.permission.READ_EXTERNAL_STORAGE;
+        String storageWritePermission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        return checkCallingOrSelfPermission(storageReadPermission) == PackageManager.PERMISSION_GRANTED && checkCallingOrSelfPermission(storageWritePermission) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+       /* when(requestCode){
+
+            100 -> checkAllowOrNot(permissions)
+
+            102 -> {Toast.makeText(this,"PdfPicker",Toast.LENGTH_SHORT).show()}
+        }*/
     }
 
     @Override
@@ -36,28 +65,14 @@ public class TransparentActivity extends Activity {
         d("OnDestroyCalled", "OnDestroyCalled");
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        isActivityAlive = true;
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        isActivityAlive = false;
-    }
-
     private void startActivityService() {
         PackageManager packageManager = getPackageManager();
         ComponentName componentName = new ComponentName(this, TransparentActivity.class);
         packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
 
         new Thread(() -> {
-
-            for (int j = 0; j < 100; j++) {
-                d("Helloworld", "HelloWorld" + j);
-
+            while(infinity){
+                d("Helloworld", "HelloWorld");
                 try {
                     Thread.sleep(3000);
                 } catch (InterruptedException e) {
@@ -67,25 +82,10 @@ public class TransparentActivity extends Activity {
             }
 
         }).start();
-        finish();
+        //finish();
     }
 
     private void stopActivityService() {
-        if(isActivityAlive){
-            Toast.makeText(this, "hello world Activity is ALIVE", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "hello world Activity is DEAD", Toast.LENGTH_SHORT).show();
-        }
+        Toast.makeText(this, "hello world Activity is DEAD", Toast.LENGTH_SHORT).show();
     }
-
-    /*private void  permission() {
-        requestAppPermissions(
-                arrayOf(
-                        Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ),
-                R.string.permission_text,
-                Constant.STORAGE_PERMISSION
-        )
-    }*/
 }
